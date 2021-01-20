27857d9fc2a35b3d7c33d1cd661b5fe04b044a88
David Whiting <davw@apache.org>
2021-01-20 18:17:10 +0100

    Add GPL copyright notices

--- ParticleSimulation.h
+++ ParticleSimulation.h
@@ -1,6 +1,19 @@
-//
-// Created by David Whiting on 2020-12-11.
-//
+/*
+    Copyright 2021 David Whiting
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #ifndef PARTICLES_PLUGIN_PARTICLESIMULATION_H
 #define PARTICLES_PLUGIN_PARTICLESIMULATION_H
